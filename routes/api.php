d2327026ff67bd299063c8d07583f0036b2314d6
João Paulo Abdala Bohaczk <okiabdala@gmail.com>
2025-12-24 11:44:08 -0300

    implementing idempotency keys for transfer route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TransferController;
 use App\Http\Controllers\TransferStoreController;
 use App\Http\Controllers\WalletController;
+use App\Http\Middleware\IdempotencyKeysMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
@@ -12,7 +13,8 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::apiResource('/wallet', WalletController::class)->only(['index', 'show']);
 Route::post('/wallet/{wallet}/deposit', [WalletController::class, 'deposit'])->name('wallet.deposit');
 
-Route::get('/transfers', [TransferController::class, 'index']);
-Route::get('/transfers/{transfer}', [TransferController::class, 'show']);
+Route::get('/transfer', [TransferController::class, 'index']);
+Route::get('/transfer/{transfer}', [TransferController::class, 'show']);
 
-Route::post('/transfer', TransferStoreController::class);
+Route::post('/transfer', TransferStoreController::class)
+    ->middleware(IdempotencyKeysMiddleware::class);
